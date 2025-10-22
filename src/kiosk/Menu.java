f2dc555f0606f1c09ba72a384048f0e3ec7654a1
junbeom95-lee <junbeom95@gmail.com>
2025-10-22 20:01:37 +0900

    chore(lv4): lv4 구현 설계 완성
    1. MenuItem 클래스를 List로 관리
    2. List에 들어있는 MenuItem을 순차적으로 보여주는 함수
    3. List를 리턴하는 함수
    4. 구조에 맞게 함수를 선언해놓고 가져다 사용할 것

--- src/kiosk/Menu.java
+++ src/kiosk/Menu.java
@@ -2,6 +2,12 @@ package kiosk;
 
 public class Menu { //MenuItem 클래스를 관리하는 클래스
     //속성
+    //TODO MenuItem 클래스를 List로 관리
+
     //생성자
+
     //기능
+    //TODO List에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    //TODO List를 리턴하는 함수
+    //TODO 구조에 맞게 함수를 선언해놓고 가져다 사용할 것
 }
