606226da49bbddc38c9befbeaa0734d7319554db
junbeom95-lee <junbeom95@gmail.com>
2025-10-22 18:36:02 +0900

    chore(lv3): Kiosk 클래스 코드 설계
    TODO
    1. MenuItem을 관리하는 리스트가 필드로 존재
     a. Main클래스에 있는 리스트를 옮기기
    2. List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당
     a. Kiosk(List<MenuItem> list)로 생성자 하나 생성 예정
    3. main 함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리
     a. main의 메서드 대부분 Kiosk클래스로 옮기기

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -2,6 +2,10 @@ package kiosk;
 
 public class Kiosk {    //프로그램 순서 및 흐름 제어를 담당하는 클래스
     //속성
+    //TODO MenuItem을 관리하는 리스트가 필드로 존재
     //생성자
+    //TODO List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당
+    //TODO Kiosk 객체를 생성하고 사용하는 main 함수에서 객체를 생성할 때 값을 넘겨줌
     //기능
+    //TODO main 함수에서 관리하던 입력과 반복문 로직은 이제 start 함수를 만들어 관리
 }
