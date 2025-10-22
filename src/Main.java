fa410ae65aa0f58bf9e493056974268b7781d8cd
junbeom95-lee <junbeom95@gmail.com>
2025-10-22 18:28:53 +0900

    feat(lv2): 선택한 메뉴 출력
    1. 선택한 메뉴를 골랐을 시 출력
     a. 다른 숫자를 입력했을 시 indexoutofrangeexception 발생하여 if문을 통해 다시 입력해달는 출력 후 다시 진행
     b.exit -> selectNum 으로 변수명 변경

--- src/Main.java
+++ src/Main.java
@@ -19,26 +19,26 @@ public class Main {
         menuItemList.add(new MenuItem("새우햄버거", 13000, "이것이 버거인가 새우인가 수원짱치즈버거"));
 
         //1. 메뉴 입력받기
-        while (true) {
-            //1. 메뉴 설정
-            System.out.print("메뉴의 이름을 입력해주세요: ");
-            String name = sc.nextLine();
-            System.out.print("메뉴의 가격을 입력해주세요: ");
-            int price = getIntInput();
-            System.out.print("메뉴의 설명을 입력해주세요: ");
-            String info = sc.nextLine();
-            //2. 입력 받은 값을 객체에 담아 리스트에 저장
-            MenuItem menuItem = new MenuItem(name, price, info);
-            menuItemList.add(menuItem);
-
-            //3. 더 저장할 지 물어봄
-            System.out.print("입력을 종료하시겠습니까? ");
-            int n = getIntInput();
-            if (0 == n) break;
-        }
+//        while (true) {
+//            //1. 메뉴 설정
+//            System.out.print("메뉴의 이름을 입력해주세요: ");
+//            String name = sc.nextLine();
+//            System.out.print("메뉴의 가격을 입력해주세요: ");
+//            int price = getIntInput();
+//            System.out.print("메뉴의 설명을 입력해주세요: ");
+//            String info = sc.nextLine();
+//            //2. 입력 받은 값을 객체에 담아 리스트에 저장
+//            MenuItem menuItem = new MenuItem(name, price, info);
+//            menuItemList.add(menuItem);
+//
+//            //3. 더 저장할 지 물어봄
+//            System.out.print("입력을 종료하시겠습니까? ");
+//            int n = getIntInput();
+//            if (0 == n) break;
+//        }
 
         //2. 메뉴 출력하기
-        int exit;
+        int selectNum;
         do {
             //메뉴 출력
             System.out.println("[ SHAKESHACK MENU ]");
@@ -48,10 +48,17 @@ public class Main {
             }
             System.out.printf("0. %-13s   | 종료\n", "종료");
 
-            //메뉴 선택
-            exit = getIntInput();   //InputMismatchException 오류 발생 가능
+            //메뉴 선택 - 숫자를 입력 받기
+            selectNum = getIntInput();
 
-        } while (0 != exit);
+            if(menuItemList.size() < selectNum) {
+                System.out.println("다시 입력해 주세요.");
+            } else if (0 != selectNum) {
+                //입력된 숫자에 따른 처리
+                MenuItem pickItem = menuItemList.get(selectNum - 1);
+                System.out.println("선택한 메뉴 -> 이름: " + pickItem.getName() + " 가격: " + (float) pickItem.getPrice() * 0.001 + " 설명: " + pickItem.getInfo());
+            }
+        } while (0 != selectNum);
 
         sc.close(); //Scanner 종료
         System.out.println("프로그램을 종료합니다.");
