d45747a5a93ba6bad48a97aa6cd57e5061645b6f
junbeom95-lee <junbeom95@gmail.com>
2025-10-22 16:44:22 +0900

    feat(lv1): MenuItem 속성 생성자 기능 추가
    1. 속성 : 이름(name), 가격(price), 설명(info)
    2. 생성자 : 속성들을 전부 매개변수로 받는 생성자 생성
    3. getter : 속성의 값을 조회할 수 있는 기능

--- src/kiosk/MenuItem.java
+++ src/kiosk/MenuItem.java
@@ -2,6 +2,26 @@ package kiosk;
 
 public class MenuItem { //세부 메뉴 속성 가지는 클래스
     //속성
+    private String name;    //이름
+    private int price;      //가격
+    private String info;    //설명
+
     //생성자
+    public MenuItem(String name, int price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
     //기능
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
 }
