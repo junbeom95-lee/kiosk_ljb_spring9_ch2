08cd2027dde39dbe244611e5a2cc3bff7e7846e9
junbeom95-lee <junbeom95@gmail.com>
2025-10-25 00:14:02 +0900

    refactor(lv4): Kiosk 클래스 구조 개편
    1. List<MenuItem> -> Menu클래스에서 관리하도록 변경
     a. 여러 Menu들을 받을 List가 필요
     b. List를 활용하여 Menu 클래스를 관리
    2. 생성자 매개 변수 변경
     a. List<MenuItem> list -> List<Menu> category
     b. Menu를 담은 List를 카테고리화 분별
     c. Menu 클래스 속성 (String, List<MenuItem>)
    3. String currentMenu
     a. 현재 메뉴를 표현하기 위한 문자열
     b. 지금은 가독성이 떨어지는 부분
     c. 추후 enum으로 변경 예정
    4. switch case 문 사용
     a. 메인 메뉴일 때 List<Menu>에서 꺼내는 단계
      i. Menu의 String name을 통해 메뉴 구성 출력 ex) BURGER, DRINK etc
     b. currentMenu를 통해 해당 메뉴로 전환
     c. 유저가 넣은 숫자가 0일때와 MAIN 메뉴에서만 종료 가능
    5. selectNumber를 통해 컨트롤
     a. Menu를 선택할 수 있음
     b. 이 변수를 통해 메뉴와 메뉴 아이템을 고르거나 종료 가능

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -7,13 +7,12 @@ import java.util.Scanner;
 public class Kiosk {    //프로그램 순서 및 흐름 제어를 담당하는 클래스
     //속성
     //MenuItem을 관리하는 리스트가 필드로 존재
-    List<MenuItem> menuItemList;
-    static Scanner sc = new Scanner(System.in);             //Scanner 선언
+    private final Scanner sc = new Scanner(System.in);             //Scanner 선언
+    private final List<Menu> category;
 
     //생성자
-    //List<MenuItem> menuItems 는 Kiosk 클래스 생성자를 통해 값을 할당
-    public Kiosk(List<MenuItem> list) {
-        this.menuItemList = list;
+    public Kiosk(List<Menu> category) {
+        this.category = category;
     }
 
     //기능
@@ -24,28 +23,115 @@ public class Kiosk {    //프로그램 순서 및 흐름 제어를 담당하는 
      */
     public void start() {
 
-        //2. 메뉴 출력하기
-        int selectNum;
+        String currentMenu = "MAIN";    //현재 ~~ 메뉴 ENUM
+        int selectNumber = 0;             //유저가 고른 숫자
+        Menu menu;
+        List<MenuItem> itemList;
+        //반복문 시작
         do {
             //메뉴 출력
-            System.out.println("[ 수원짱버거 MENU ]");
-            for(int i = 0 ; i < menuItemList.size(); i++) { //반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
-                MenuItem mI = menuItemList.get(i);
-                System.out.printf("%d. %-13s | W %5.1f | %s\n", i + 1, mI.getName(), (float)mI.getPrice() * 0.001, mI.getInfo());
-            }
-            System.out.printf("0. %-13s  | 종료\n", "종료");
+            switch (currentMenu) {
+                case "MAIN":    //메인 메뉴
 
-            //메뉴 선택 - 숫자를 입력 받기
-            selectNum = getIntInput();
+                    //List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+                    System.out.println("[ MAIN MENU ]");
+                    for(int i = 0; i < category.size(); i++) {
+                        System.out.printf("%d. %-13s\n", i + 1,  category.get(i).getName());
+                    }
+                    System.out.println("0. 종료   | 종료");
 
-            if(menuItemList.size() < selectNum) {   //유효하지 않은 입력에 대해 오류 메시지를 출력
-                System.out.println("번호를 잘 보시고 입력해주세요.");
-            } else if (0 != selectNum) {
-                //입력된 숫자에 따른 처리
-                MenuItem pickItem = menuItemList.get(selectNum - 1);
-                System.out.println("선택한 메뉴 -> 이름: " + pickItem.getName() + " 가격: " + (float) pickItem.getPrice() * 0.001 + " 설명: " + pickItem.getInfo());
+                    //숫자 입력 받기 - 메뉴 선택
+                    selectNumber = getIntInput();
+
+                    //해당 메뉴로 전환
+                    if(selectNumber == 1) currentMenu = "BURGER";
+                    else if (selectNumber == 2) currentMenu = "DRINK";
+                    else if (selectNumber == 3) currentMenu = "SIDE";
+                    else if (selectNumber == 0) System.out.println("종료하겠습니다.");
+                    else System.out.println("잘못된 숫자입니다.");
+                    break;
+
+                case "BURGER":  //버거 메뉴
+
+                    System.out.println("[ BURGER MENU ]");
+                    //입력받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                    //List<Menu>에 인덱스로 접근하면 Menu만 추출 가능
+                    menu =  category.get(selectNumber -1);
+
+                    //Menu가 가진 List<MenuItem>을 반복문을 활용하여 BURGER 메뉴 출력
+                    itemList = menu.getMenuItemList();
+                    menu.printMenuItem(itemList);
+
+                    //숫자 입력 받기 - 메뉴 선택
+                    selectNumber = getIntInput();
+
+                    if(itemList.size() < selectNumber || selectNumber < 0) {
+                        System.out.println("번호를 잘 보시고 입력해주세요.");
+                    } else if (0 == selectNumber) {
+                        currentMenu = "MAIN";
+                        selectNumber = -1;
+                    } else {
+                        MenuItem pickItem = itemList.get(selectNumber - 1);
+                        System.out.println("선택한 메뉴 -> 이름: " + pickItem.getName() + " 가격: " + (float) pickItem.getPrice() * 0.001 + " 설명: " + pickItem.getInfo());
+                        currentMenu = "MAIN";
+                    }
+                    break;
+                case "DRINK":
+
+                    System.out.println("[ DRINK MENU ]");
+                    //입력받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                    menu =  category.get(selectNumber -1);
+
+                    //Menu가 가진 List<MenuItem>을 반복문을 활용하여 DRINK 메뉴 출력
+                    itemList = menu.getMenuItemList();
+                    menu.printMenuItem(itemList);
+
+
+                    //숫자 입력 받기 - 메뉴 선택
+                    selectNumber = getIntInput();
+
+                    if(itemList.size() < selectNumber || selectNumber < 0) {
+                        System.out.println("번호를 잘 보시고 입력해주세요.");
+                    } else if (0 == selectNumber) {
+                        currentMenu = "MAIN";
+                        selectNumber = -1;
+                    } else {
+                        MenuItem pickItem = itemList.get(selectNumber - 1);
+                        System.out.println("선택한 메뉴 -> 이름: " + pickItem.getName() + " 가격: " + (float) pickItem.getPrice() * 0.001 + " 설명: " + pickItem.getInfo());
+                        currentMenu = "MAIN";
+                    }
+                    break;
+                case "SIDE":
+
+                    System.out.println("[ SIDE MENU ]");
+                    //입력받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+                    menu =  category.get(selectNumber -1);
+
+                    //Menu가 가진 List<MenuItem>을 반복문을 활용하여 SIDE 메뉴 출력
+                    itemList = menu.getMenuItemList();
+                    menu.printMenuItem(itemList);
+
+                    //숫자 입력 받기 - 메뉴 선택
+                    selectNumber = getIntInput();
+
+                    if(itemList.size() < selectNumber || selectNumber < 0) {
+                        System.out.println("번호를 잘 보시고 입력해주세요.");
+                    } else if (0 == selectNumber) {
+                        currentMenu = "MAIN";
+                        selectNumber = -1;
+                    } else {
+                        MenuItem pickItem = itemList.get(selectNumber - 1);
+                        System.out.println("선택한 메뉴 -> 이름: " + pickItem.getName() + " 가격: " + (float) pickItem.getPrice() * 0.001 + " 설명: " + pickItem.getInfo());
+                        currentMenu = "MAIN";
+                    }
+                    break;
+                default:
+                    System.out.println("접근 불가능한 menu입니다.");
+                    System.out.println(" main 메뉴로 돌아갑니다. ");
+                    currentMenu = "MAIN";
+                    break;
             }
-        } while (0 != selectNum);
+        } while (0 != selectNumber || !"MAIN".equals(currentMenu));
 
         //프로그램 종료
         sc.close(); //Scanner 종료
