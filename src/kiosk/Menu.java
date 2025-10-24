c979101a6a36a5a6ac21bdd1ec80ded5776b160d
junbeom95-lee <junbeom95@gmail.com>
2025-10-24 23:37:32 +0900

    feat(lv4): Menu클래스 구현
    1. 속성
     a. String name : 카테고리 이름 필드 ex) BURGER, DRINK, SIDE
     b. List<MenuItem> menuItemList : Kiosk 클래스가 관리 적절하지 않아 Menu 클래스가 관리하도록 변경 ex) 버거 리스트, 드링크 리스트
    2. 생성자: 속성들을 전부 받는 생성자 생성
    3. 기능 : getter - 메뉴 카테고리 이름을 반환하는 메서드 구현

--- src/kiosk/Menu.java
+++ src/kiosk/Menu.java
@@ -5,32 +5,27 @@ import java.util.List;
 public class Menu { //MenuItem 클래스를 관리하는 클래스
     //속성
     //MenuItem 클래스를 List로 관리
-    List<MenuItem> menuItemList;
+    private String name;                    //메뉴 이름
+    private List<MenuItem> menuItemList;    //사이드 메뉴 리스트
 
     //생성자
-    public Menu(List<MenuItem> list) {
-        this.menuItemList = list;
+    public Menu(String name, List<MenuItem> menuItemList) {
+        this.name = name;
+        this.menuItemList = menuItemList;
     }
 
     //기능
     /**
-     * List에 들어있는 MenuItem을 순차적으로 보여주는 함수
-     */
-    public void printMenuItemList() {
-        System.out.println("[ 수원짱버거 MENU ]");
-        for(int i = 0 ; i < menuItemList.size(); i++) { //반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
-            MenuItem mI = menuItemList.get(i);
-            System.out.printf("%d. %-13s | W %5.1f | %s\n", i + 1, mI.getName(), (float)mI.getPrice() * 0.001, mI.getInfo());
-        }
-        System.out.printf("0. %-13s  | 종료\n", "종료");
-    }
-
-    /**
-     * List를 리턴하는 함수
+     * MenuItemList 리턴하는 함수
      */
     public List<MenuItem> getMenuItemList() {
         return menuItemList;
     }
 
-    //TODO 구조에 맞게 함수를 선언해놓고 가져다 사용할 것
+    /**
+     * Menu의 이름을 리턴하는 함수
+     */
+    public String getName() {
+        return name;
+    }
 }
