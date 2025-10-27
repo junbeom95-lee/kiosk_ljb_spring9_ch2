d4e5308bb1d4a26d127fa8a9d63b8a086c3a1fb6
junbeom95-lee <junbeom95@gmail.com>
2025-10-27 21:03:54 +0900

    feat(challenge_lv1): Cart 클래스 구현
    1. 장바구니에 메뉴아이템 추가기능 (addMenuItem)
     a. param: MenuItem 메뉴아이템, int 수량
     b. 수량이 0으로 들어오면 메뉴아이템 Map에서 삭제
     c. 0이 아니면 수량 수정
    2. 메뉴아이템이 장바구니에 담겨있는지 확인 (checkMenuItemEa)
     a. param: MenuItem 메뉴아이템
     b. return: 있으면 수량을 반환 없으면 0 반환
     c. 이 기능을 통해 출력되는 메뉴얼이 달라짐
    3. 장바구니에 물건 확인 (checkCart)
     a. 장바구니가 비어있으면 true를 반환
     b. 이 기능을 통해 ORDERS 메뉴를 보여줄지 컨트롤함
    4. 장바구니에 담긴 모든 메뉴 총 금액 계산 (getTotal)
     a. return: 총 금액
    5. 장바구니에 들어있는 MenuItem을 모두 보여주는 메서드 (printMenuItemList)

--- src/kiosk/Cart.java
+++ src/kiosk/Cart.java
@@ -3,19 +3,76 @@ package kiosk;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Cart { //장바구니 기능
+public class Cart { //MenuItem을 장바구니에 담는 기능을 가진 클래스
     //속성
-    private Map<Menu, Integer> cart = new HashMap<>();
+    private final Map<MenuItem, Integer> cart;  //장바구니
 
     //생성자
+    public Cart() {
+        this.cart = new HashMap<>();
+    }
 
     //기능
 
-    //TODO
-    //장바구니 추가 기능 (Menu 메뉴 (메뉴명, 수량, 가격 ,정보), Int 수량)HashMap.put()사용
-    //장바구니 수정 기능 (Menu 메뉴 (키 값), int 수정 수량) -> 0이면 장바구니 삭제 기능 HashMap.remove()(Menu 메뉴 (키 값))
-    //유효하지 않은 메뉴 번호 입력 -> Kiosk 클래스의 printSelectMenuItem 이미 구현
-    //장바구니에 물건 확인 <- 물건이 들어있으면 Order Menu 추가로 출력
-    //장바구니에 담긴 모든 메뉴와 총 금액 출력
+    /**
+     * 장바구니에 메뉴아이템 추가 기능
+     * @param menuItem 메뉴 아이템
+     * @param ea       수량
+     */
+    public void addMenuItem(MenuItem menuItem, int ea) {
+        //장바구니 추가 기능
+        if (0 == ea) cart.remove(menuItem); //수량을 0으로 바꾸면 메뉴아이템 삭제
+        else cart.put(menuItem, ea);        //메뉴 아이템 수량 수정
+    }
+
+    /**
+     * 메뉴아이템이 장바구니에 담겨있는지 확인
+     * @param menuItem 메뉴아이템 <- 키 값
+     * @return 있으면 value를 반환 없으면 0 반환
+     */
+    public int checkMenuItemEa(MenuItem menuItem) {
+        return cart.getOrDefault(menuItem, 0);
+    }
+
+    /**
+     * 장바구니에 물건 확인
+     * @return 물건 없으면 true
+     */
+    public boolean checkCart() {
+        return cart.isEmpty();
+    }
+
+    /**
+     * 장바구니에 담긴 모든 메뉴 총 금액 계산
+     * @return 총 금액
+     */
+    public int getTotal() {
+        //총 가격
+        int total = 0;
+
+        for (MenuItem menuItem : cart.keySet()) {
+            int ea = cart.get(menuItem);
+            total += menuItem.getPrice() * ea;
+        }
+        return total;
+    }
+
+    /**
+     * 장바구니(Map)에 들어있는 MenuItem을 순차적으로 보여주는 메서드
+     */
+    public void printMenuItemList() {
+        for (MenuItem menuItem : cart.keySet()) {
+            int ea = cart.get(menuItem);
+            // 메뉴아이템 출력 메뉴 이름 | 가격 | 설명
+            System.out.printf("%-13s | W %5.1f | %5dea | %s\n", menuItem.getName(), (float) menuItem.getPrice() * 0.001 * ea, ea, menuItem.getInfo());
+        }
+    }
+
+    /**
+     * 장바구니 초기화 기능
+     */
+    public void clearCart() {
+        cart.clear();
+    }
 
 }
